f56abe7a849ebe1cb2fea94066fae8ab48204a7f
ZiedYT <65912881+ZiedYT@users.noreply.github.com>
2023-12-27 18:19:51 +0100

    Add files via upload

---
+++ alertStreamElement.js
@@ -0,0 +1,34 @@
+var socket = null
+var gifted = {}
+function subscription() {
+	socket = io('https://realtime.streamelements.com', {
+		transports: ['websocket']
+	});
+
+	socket.on('connect', () => {
+		console.log('Successfully connected to the websocket')
+		socket.emit('authenticate', { method: 'jwt', token: token })
+	})
+
+	socket.on('event', (data) => {
+		amount = data.data.amount
+		console.log(data)
+		if (data.type == 'subscriber') {
+			if(data.data.tier == "2000")
+				current = current + 6
+			else if(data.data.tier == "3000")
+				current = current + 17.5
+			else
+				current = current + 2.5
+		}
+		else if (data.type == 'cheer') {
+			current = current + data.data.amount/100
+		}
+		else if (data.type == 'tip'){
+			current = current + data.data.amount
+		}
+		update()
+	});
+}
+
+subscription()
---
+++ script.js
@@ -0,0 +1,81 @@
+var element
+var bags
+var current = 0
+var target = 500
+document.addEventListener("DOMContentLoaded", function() {
+    element = document.querySelector('#money');
+    element.textContent = "DROP LEGO SET: "+ current+"/"+ target 
+
+});    
+function update(){
+  element.textContent = "DROP LEGO SET: "+ current+"/"+ target 
+}
+
+
+
+var urlParams = new URLSearchParams(window.location.search);
+const channelname = urlParams.get('channel'); 
+const oauthToken =urlParams.get('key');  
+const token =urlParams.get('jwt');  
+
+console.log(token)
+current = parseInt( localStorage.getItem("current-progress-bar-"+channelname) || 0) ;
+console.log(current)
+
+if(current==NaN)
+  current=0;
+// Define configuration options
+const opts = {
+  identity: {
+    username: 'botreading',
+    password: oauthToken
+  },
+  channels: [
+    channelname
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+  var text = msg.trim();
+  console.log(msg)
+  if( context.username.toLowerCase() == channelname.toLowerCase() || context.mod){
+    if(text.split(" ")[0] == "!goal"){
+        var bagnumb = parseInt(text.split(" ")[1]);
+        if( !isNaN(bagnumb) ){
+            target = bagnumb;
+            console.log(bagnumb)
+            element.textContent = "DROP LEGO SET: "+ current+"/"+ target 
+        }
+    }
+    else if(text.split(" ")[0] == "!current"){
+        var bagnumb = parseInt(text.split(" ")[1]);
+        if( !isNaN(bagnumb) ){
+            current = bagnumb;
+            element.textContent = "DROP LEGO SET: "+ current+"/"+ target 
+        }
+    }
+    else if(text.split(" ")[0] == "!clear"){
+      var bagnumb = parseInt(text.split(" ")[1]);
+      if( !isNaN(bagnumb) ){
+          current = 0;
+          element.textContent = "DROP LEGO SET: "+ current+"/"+ target 
+      }
+  }
+  }
+}
+
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
+
